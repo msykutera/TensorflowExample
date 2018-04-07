7ffa59623807a1c7d21e710d9bf0ef846d4b2028
Michał Sykutera <msykutera@gmail.com>
2018-04-07 18:18:06 +0200

    Working version

--- TensorflowExample/Controllers/ValuesController.cs
+++ TensorflowExample/Controllers/ValuesController.cs
@@ -1,44 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ExampleCommon;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using TensorFlow;
 
 namespace TensorflowExample.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private IEnumerable<string> ReadLines(FileStream stream)
         {
-            return new string[] { "value1", "value2" };
+            using (var reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public string Get(IFormFile file)
         {
-        }
+            var graph = new TFGraph();
+            var stream = new MemoryStream();
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+            FileStream fileStream = new FileStream(@"C:\Users\syku\source\repos\TensorflowExample\TensorflowExample\tensorflow_inception_graph.pb", FileMode.Open);
+            fileStream.CopyTo(stream);
+            var model = stream.ToArray();
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            var labelsFile = new FileStream(@"C:\Users\syku\source\repos\TensorflowExample\TensorflowExample\imagenet_comp_graph_label_strings.txt", FileMode.Open);
+            var labels = ReadLines(labelsFile).ToList();
+
+            graph.Import(model, "");
+            using (var session = new TFSession(graph))
+            {
+                var tensor = ImageUtil.CreateTensorFromImageFile(file);
+
+                var runner = session.GetRunner();
+                runner.AddInput(graph["input"][0], tensor).Fetch(graph["output"][0]);
+                var output = runner.Run();
+                // output[0].Value() is a vector containing probabilities of
+                // labels for each image in the "batch". The batch size was 1.
+                // Find the most probably label index.
+
+                var result = output[0];
+                var rshape = result.Shape;
+                if (result.NumDims != 2 || rshape[0] != 1)
+                {
+                    var shape = "";
+                    foreach (var d in rshape)
+                    {
+                        shape += $"{d} ";
+                    }
+                    shape = shape.Trim();
+                    Console.WriteLine($"Error: expected to produce a [1 N] shaped tensor where N is the number of labels, instead it produced one with shape [{shape}]");
+                    Environment.Exit(1);
+                }
+
+                var bestIdx = 0;
+                float best = 0;
+
+                var probabilities = ((float[][])result.GetValue(jagged: true))[0];
+                for (int i = 0; i < probabilities.Length; i++)
+                {
+                    if (probabilities[i] > best)
+                    {
+                        bestIdx = i;
+                        best = probabilities[i];
+                    }
+                }
+
+                return $"{file} best match: [{bestIdx}] {best * 100.0}% {labels[bestIdx]}";
+            }
         }
     }
 }
